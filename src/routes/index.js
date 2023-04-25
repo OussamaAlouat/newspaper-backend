923cf1c7dd9390ee4e16be9e1a737e5335d0bdcd
OussamaAlouat <al317257@uji.es>
2023-04-26 00:06:32 +0200

    FEAT: Add route to get all newspaper

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { index } from '../controller'
+import { getNewsPappers } from '../controller/newsPapper';
+import newspaper from '../model/newspaper';
 
 export default () => {
   const routes = Router();
   routes.get('/',
     (req, res) => index(req, res)
   );
+
+  routes.get('/newspaper', (req, res) => getNewsPappers(req, res))
 
   return routes;
 }
