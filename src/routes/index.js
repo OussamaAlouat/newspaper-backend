7860949f03927a653e7a36d0685d1a094fcfbcac
OussamaAlouat <al317257@uji.es>
2023-04-26 20:30:37 +0200

    FEAT: added validators

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import { index } from '../controller'
 import { getNewsPapers, postNewsPaper } from '../controller/newsPapper';
 
+import { check } from "express-validator";
+import { postCheckValidation } from "../middleware/validation";
+
 export default () => {
   const routes = Router();
   routes.get('/',
@@ -9,7 +12,14 @@ export default () => {
   );
 
   routes.get('/newspaper', (req, res) => getNewsPapers(req, res))
-  routes.post('/newspaper', (req, res) => postNewsPaper(req, res))
+  routes.post('/newspaper',     [
+    check('title').isLength({min: 4}),
+    check('abstract').isLength({min: 2}),
+    check('creation_date').exists(),
+    check('publisher').exists()
+  ],
+  (req, res, next) => postCheckValidation(req, res, next),
+  (req, res) => postNewsPaper(req, res))
 
   return routes;
 }
