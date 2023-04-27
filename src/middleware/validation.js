1f51c0f0c16c0d3c298313b18ffa58f3d2289ca6
OussamaAlouat <al317257@uji.es>
2023-04-27 22:24:01 +0200

    FIX: identation

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,12 +1,13 @@
 import { validationResult } from "express-validator";
 
 const postCheckValidation = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array()})
-    }
-    next()
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()})
+  }
+  
+  next()
 };
 export {
-    postCheckValidation
+  postCheckValidation
 }
