1b304ee630c5eee5b3a00775c6f19c24785c28e0
OussamaAlouat <al317257@uji.es>
2023-04-28 09:31:26 +0200

    FIX: change controller for suport new schemas

--- src/controller/newsPapper.js
+++ src/controller/newsPapper.js
@@ -1,53 +1,74 @@
-import NewsPaper from '../model/newspaper';
-
+import { v4 } from 'uuid';
+import { publisherModel, newsPaperModel } from '../model/newspaper';
 
 const getNewsPapers = (req, res) => {
-  NewsPaper.find()
-    .then((response) => {
-      res.status(200).json({data: response})
-    })
+  newsPaperModel.find().populate('publisher').then((response) => {
+    res.status(200).json({data: response})
+  })
 };
+
+const saveNewsPaper = (body, res) => {
+  const newsPaperNew = new newsPaperModel(body);
+  newsPaperNew.save().then((data) => {
+    const returnData = {
+      data: data,
+      message:'Document created correctly'
+    };
+
+    res.status(201).json(returnData)
+  }).catch((err) => {
+    console.log(err);
+    res.json(err)
+  })
+}
 
 const postNewsPaper = (req, res) => {
   const { title, image , abstract, creation_date, languages, publisher, link } = req.body;
+  publisherModel.findOne({ name: publisher.name }).then(resp => {
+    if(resp) {
+      newsPaperModel.find({ title, creation_date, publisher: resp })
+      .then((response) => {
+        if (response.length > 0) {
+          const errorMessage = 'This document already exists on database';
+          res.status(409).json({message: errorMessage});
+        } else {
+          const body = {
+            title, image,
+            abstract,
+            creation_date,
+            languages,
+            publisher: resp._id,
+            link
+          };
+          saveNewsPaper(body, res)
+        }
+      })
+    } else {
+      const _id = v4();
+      const newPubliser = new publisherModel({
+        _id,
+        name: publisher.name,
+        joined_date: new Date().toString()
+      });
 
-  //If there are one document with the same title, creation_date and publisher
-  //we will not put in our database.
-  NewsPaper.find({ title, creation_date, publisher })
-    .then((response) => {
-      if (response.length > 0) {
-        const errorMessage = 'This document already exists on database';
-        res.status(409).json({message: errorMessage});
-      } else {
-        const newsPaperNew = new NewsPaper({
+      newPubliser.save().then(data => {
+        const body = {
           title,
           image,
           abstract,
           creation_date,
           languages,
-          publisher,
-          link
-        });
-        
-
-        newsPaperNew.save().then((data) => {
-          const returnData = {
-            data: data,
-            message:'Document created correctly'
-          };
-
-          res.status(201).json(returnData)
-        }).catch((err) => {
-          console.log(err);
-          res.json(err)
-        })
-      }
-    })
+          publisher: _id, link
+        };
+        saveNewsPaper(body, res);
+      })
+    }
+  })
 };
 
 const deleteNewsPaper = (req, res ) => {
   const { id } = req.body;
-  NewsPaper.findByIdAndDelete(id).then((resp) => {
+  newsPaperModel.findByIdAndDelete(id).then((resp) => {
     if(resp !== null) {
       res.json(resp);
     } else {
