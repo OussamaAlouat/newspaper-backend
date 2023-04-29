b6753988a654dd9b53fd82c9990151caccd4980b
OussamaAlouat <al317257@uji.es>
2023-04-29 12:50:03 +0200

    FIX: change the return value of '/' end-point

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,5 +1,18 @@
 const index = (req, res) => {
-  const response =  {data : {message: 'Server up!!'}};
+  const response = {
+    routes: {
+      '/newspaper': {
+        'get': 'To get a newspaper',
+        'post': 'To add newspaper'
+      },
+      '/newspaper/:id': {
+        'get': 'To get only one newspaper',
+        'delete': 'To delete a newspaper',
+        'put': 'Update a mewspaper'
+      }
+    },
+    message: 'Please make sure that you are readed the README.md'
+  }
   res.json(response);
 };
 
