553b428b9c10253ce509f2636d4fa36e6226b78a
OussamaAlouat <al317257@uji.es>
2023-05-01 20:20:29 +0200

    FIX: Update index response

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,14 +1,24 @@
 const index = (req, res) => {
   const response = {
     routes: {
-      '/newspaper': {
-        'get': 'To get a newspaper',
-        'post': 'To add newspaper'
+      '/newspaper/': {
+        post: "Create a new newspaper"
       },
+      '/newspaper/title?': {
+        'get': 'To get all newspaper that starts with the provided "title"'
+      },
+      '/newspaper/all': {
+        'get': 'To get a newspaper',
+      },
+
       '/newspaper/:id': {
-        'get': 'To get only one newspaper',
         'delete': 'To delete a newspaper',
         'put': 'Update a mewspaper'
+      },
+
+      '/newspaper/id/:id': {
+        'get': 'To get only one newspaper',
+
       }
     },
     message: 'Please make sure that you are readed the README.md'
